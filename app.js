fe3fe3d24b98a253f53b5e0849595dbabf228168
Hua0720 <stella573021@gmail.com>
2022-11-13 17:53:07 +0800

    feat: add views/edit.hbs

--- app.js
+++ app.js
@@ -54,13 +54,6 @@ app.use((req, res, next) => {
   next()
 })
 
-// 加入一組 middleware
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  next()
-})
-
 // 將 request 導入路由器
 app.use(routes)
 
